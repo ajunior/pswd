ac18381f9fbbaaf67c8f3fde42761a57d89f3eb8
A. Junior <jr@ieee.org>
2022-09-03 07:49:56 -0300

    add default value for password length

--- main.nim
+++ main.nim
@@ -4,12 +4,13 @@ randomize()
 
 const
     numbers = 48 .. 57
+    PASSWD_DEFAULT_LENGTH = 4
 
 
 proc generate_password(): string =
     var
         n: int
-    for j in 1 .. 4:
+    for j in 1 .. PASSWD_DEFAULT_LENGTH:
         n = rand(numbers)
         result.add(char(n))
 
