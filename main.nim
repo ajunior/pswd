a6470d86fe88bc41d882be4fd5af57224ab44b8c
A. Junior <jr@ieee.org>
2022-09-03 08:08:31 -0300

    feat: dynamic password length

--- main.nim
+++ main.nim
@@ -1,21 +1,27 @@
-import std/random
+import os, std/random, std/strutils
 
 randomize()
 
 const
     numbers = 48 .. 57
-    PASSWD_DEFAULT_LENGTH = 4
+    PASSWD_DEFAULT_LENGTH = 6
 
 
-proc generate_password(): string =
+proc generate_password(length: int): string =
     var
         n: int
-    for j in 1 .. PASSWD_DEFAULT_LENGTH:
+    for j in 1 .. length:
         n = rand(numbers)
         result.add(char(n))
 
 
 proc main =
-    echo generate_password()
+    var length: int
+    if paramCount() == 0:
+        length = PASSWD_DEFAULT_LENGTH
+    else:
+        length = parseInt(paramStr(1))
+
+    echo generate_password(length)
 
 main()
